a0a33ae15a91b5528b5f08dd5c4d2da8a6f772bd
Mathprestes <matheus.prestes_@outlook.com>
2021-05-07 16:29:04 -0300

    Home, Navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,12 +1,17 @@
 import ImgDsDark from 'assets/img/ds_dark.svg'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {   //mesma coisa se usar a funcao no lugar
     return (
         <div className="nav d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 shadow-sm">
             <div className="container">
+                
                 <nav className="my-2 my-md-0 mr-md-3">
+                    <Link to="/">
                     <img src={ImgDsDark} alt="DevSuperior" width="200" />
+                    </Link>
                 </nav>
+                
             </div>
         </div>
     );
